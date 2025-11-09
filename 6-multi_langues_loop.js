edaa59414eda547bae13303263c091f045603478
Joseph <10326622@upsamail.edu.gh>
2025-11-09 21:07:32 +0200

    Create 6-multi_langues_loop.js

---
+++ 6-multi_langues_loop.js
@@ -0,0 +1,8 @@
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Loop through the array and print each element individually.
+languages.forEach(language => console.log(language));
